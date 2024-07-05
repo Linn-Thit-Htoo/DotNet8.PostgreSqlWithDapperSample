8f563baff4369a4729d7fec351d524f526c85575
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-05 17:33:47 +0630

    Format Code

--- DotNet8.PostgreSqlWithDapperSample/Services/DapperService.cs
+++ DotNet8.PostgreSqlWithDapperSample/Services/DapperService.cs
@@ -1,32 +1,49 @@
-﻿namespace DotNet8.PostgreSqlWithDapperSample.Services
+﻿namespace DotNet8.PostgreSqlWithDapperSample.Services;
+
+public class DapperService
 {
-    public class DapperService
+    private readonly IConfiguration _configuration;
+
+    public DapperService(IConfiguration configuration)
     {
-        private readonly IConfiguration _configuration;
+        _configuration = configuration;
+    }
 
-        public DapperService(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+    public async Task<List<T>> QueryAsync<T>(
+        string query,
+        object? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
+    {
+        using IDbConnection db = new NpgsqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
+        var lst = await db.QueryAsync<T>(query, parameters, commandType: commandType);
+        return lst.ToList();
+    }
 
-        public async Task<List<T>> QueryAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
-        {
-            using IDbConnection db = new NpgsqlConnection(_configuration.GetConnectionString("DbConnection"));
-            var lst = await db.QueryAsync<T>(query, parameters, commandType: commandType);
-            return lst.ToList();
-        }
+    public async Task<T> QueryFirstOrDefaultAsync<T>(
+        string query,
+        object? parameters = null,
+        CommandType commandType = CommandType.Text
+    )
+    {
+        using IDbConnection db = new NpgsqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
+        var item = await db.QueryFirstOrDefaultAsync<T>(
+            query,
+            parameters,
+            commandType: commandType
+        );
+        return item!;
+    }
 
-        public async Task<T> QueryFirstOrDefaultAsync<T>(string query, object? parameters = null, CommandType commandType = CommandType.Text)
-        {
-            using IDbConnection db = new NpgsqlConnection(_configuration.GetConnectionString("DbConnection"));
-            var item = await db.QueryFirstOrDefaultAsync<T>(query, parameters, commandType: commandType);
-            return item!;
-        }
-
-        public async Task<int> ExecuteAsync(string query, object parameters)
-        {
-            using IDbConnection db = new NpgsqlConnection(_configuration.GetConnectionString("DbConnection"));
-            return await db.ExecuteAsync(query, parameters);
-        }
+    public async Task<int> ExecuteAsync(string query, object parameters)
+    {
+        using IDbConnection db = new NpgsqlConnection(
+            _configuration.GetConnectionString("DbConnection")
+        );
+        return await db.ExecuteAsync(query, parameters);
     }
 }
