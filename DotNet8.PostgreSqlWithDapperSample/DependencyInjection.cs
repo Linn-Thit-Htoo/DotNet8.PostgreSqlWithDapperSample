3639572b17e6ff6c6e04d0ab549cc93a7c7eaf53
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-05 17:34:06 +0630

    Format Code

--- DotNet8.PostgreSqlWithDapperSample/DependencyInjection.cs
+++ DotNet8.PostgreSqlWithDapperSample/DependencyInjection.cs
@@ -1,28 +1,24 @@
-﻿using DotNet8.PostgreSqlWithDapperSample.Features.Blog;
-using Microsoft.Extensions.DependencyInjection;
+﻿namespace DotNet8.PostgreSqlWithDapperSample;
 
-namespace DotNet8.PostgreSqlWithDapperSample
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddFeatures(this IServiceCollection services)
     {
-        public static IServiceCollection AddFeatures(this IServiceCollection services)
-        {
-            return services.AddDataAccessService().AddBusinessLogicService().AddCustomService();
-        }
+        return services.AddDataAccessService().AddBusinessLogicService().AddCustomService();
+    }
 
-        private static IServiceCollection AddDataAccessService(this IServiceCollection services)
-        {
-            return services.AddScoped<DA_Blog>();
-        }
+    private static IServiceCollection AddDataAccessService(this IServiceCollection services)
+    {
+        return services.AddScoped<DA_Blog>();
+    }
 
-        private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
-        {
-            return services.AddScoped<BL_Blog>();
-        }
+    private static IServiceCollection AddBusinessLogicService(this IServiceCollection services)
+    {
+        return services.AddScoped<BL_Blog>();
+    }
 
-        private static IServiceCollection AddCustomService(this IServiceCollection services)
-        {
-            return services.AddScoped<DapperService>();
-        }
+    private static IServiceCollection AddCustomService(this IServiceCollection services)
+    {
+        return services.AddScoped<DapperService>();
     }
 }
--- DotNet8.PostgreSqlWithDapperSample/GlobalUsings.cs
+++ DotNet8.PostgreSqlWithDapperSample/GlobalUsings.cs
@@ -3,6 +3,7 @@
 global using System.Data;
 global using Dapper;
 global using DotNet8.PostgreSqlWithDapperSample.Enums;
+global using DotNet8.PostgreSqlWithDapperSample.Features.Blog;
 global using DotNet8.PostgreSqlWithDapperSample.Models;
 global using DotNet8.PostgreSqlWithDapperSample.Queries;
 global using DotNet8.PostgreSqlWithDapperSample.Resources;
