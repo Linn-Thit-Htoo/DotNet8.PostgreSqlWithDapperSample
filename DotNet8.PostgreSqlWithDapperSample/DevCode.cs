ce349f15146481dbad1366a2b0c5af4b87753dc9
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-05 17:34:17 +0630

    Format Code

--- DotNet8.PostgreSqlWithDapperSample/DevCode.cs
+++ DotNet8.PostgreSqlWithDapperSample/DevCode.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNet8.PostgreSqlWithDapperSample
+﻿namespace DotNet8.PostgreSqlWithDapperSample;
+
+public static class DevCode
 {
-    public static class DevCode
+    public static bool IsNullOrEmpty(this string str)
     {
-        public static bool IsNullOrEmpty(this string str)
-        {
-            return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
-        }
+        return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
     }
 }
