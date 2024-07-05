592e1d7e53240fe81c7e8dd692ecfceacf60b25f
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-05 17:30:07 +0630

    Format Code

--- DotNet8.PostgreSqlWithDapperSample/Enums/EnumStatusCode.cs
+++ DotNet8.PostgreSqlWithDapperSample/Enums/EnumStatusCode.cs
@@ -1,16 +1,14 @@
-﻿namespace DotNet8.PostgreSqlWithDapperSample.Enums
-{
-    public enum EnumStatusCode
-    {
-        None,
-        Success = 200,
-        Created = 201,
-        Accepted = 202,
-        BadRequest = 400,
-        NotFound = 404,
-        Conflict = 409,
-        Locked = 423,
-        InternalServerError = 500
-    }
+﻿namespace DotNet8.PostgreSqlWithDapperSample.Enums;
 
+public enum EnumStatusCode
+{
+    None,
+    Success = 200,
+    Created = 201,
+    Accepted = 202,
+    BadRequest = 400,
+    NotFound = 404,
+    Conflict = 409,
+    Locked = 423,
+    InternalServerError = 500
 }
