36c348f6f61355b1a819c3bac4067db0abaf344c
Linn Thit Htoo <lth1212001@gmail.com>
2024-07-05 17:33:24 +0630

    Format Code

--- DotNet8.PostgreSqlWithDapperSample/Resources/MessageResource.cs
+++ DotNet8.PostgreSqlWithDapperSample/Resources/MessageResource.cs
@@ -1,17 +1,16 @@
-﻿namespace DotNet8.PostgreSqlWithDapperSample.Resources
+﻿namespace DotNet8.PostgreSqlWithDapperSample.Resources;
+
+public class MessageResource
 {
-    public class MessageResource
-    {
-        public static string Success { get; } = "Success.";
-        public static string SaveSuccess { get; } = "Saving Successful.";
-        public static string SaveFail { get; } = "Saving Fail.";
-        public static string UpdateSuccess { get; } = "Updating Successful.";
-        public static string UpdateFail { get; } = "Updating Fail.";
-        public static string DeleteSuccess { get; } = "Deleting Successful.";
-        public static string DeleteFail { get; } = "Deleting Fail.";
-        public static string NotFound { get; } = "No Data Found.";
-        public static string Duplicate { get; } = "Duplicate Data.";
-        public static string InvalidId { get; } = "Id is invalid.";
-        public static string InvalidPageSize { get; } = "Page Size is invalid.";
-    }
+    public static string Success { get; } = "Success.";
+    public static string SaveSuccess { get; } = "Saving Successful.";
+    public static string SaveFail { get; } = "Saving Fail.";
+    public static string UpdateSuccess { get; } = "Updating Successful.";
+    public static string UpdateFail { get; } = "Updating Fail.";
+    public static string DeleteSuccess { get; } = "Deleting Successful.";
+    public static string DeleteFail { get; } = "Deleting Fail.";
+    public static string NotFound { get; } = "No Data Found.";
+    public static string Duplicate { get; } = "Duplicate Data.";
+    public static string InvalidId { get; } = "Id is invalid.";
+    public static string InvalidPageSize { get; } = "Page Size is invalid.";
 }
